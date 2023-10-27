40a02c88d37ac9734a0495b353bf75e6759194fa
xiaoliu <2558949748@qq.com>
2023-10-27 11:08:46 +0800

    提交

--- java/Demo.java
+++ java/Demo.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class Demo{
 	public static void main(String[] args) {
-		System.out.print("hello world");
+		System.out.print("请输入:");
+		Scanner sc = new Scanner(System.in);
+		String text = sc.next();
+		System.out.print(text);
 	}
 }
