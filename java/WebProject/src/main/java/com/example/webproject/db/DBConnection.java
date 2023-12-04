75ce25d73b04e9f09e9e4164def15474d508a42c
xiaoliu <2558949748@qq.com>
2023-12-04 13:58:37 +0800

    2023-12-04,13:58:36

--- java/WebProject/src/main/java/com/example/webproject/db/DBConnection.java
+++ java/WebProject/src/main/java/com/example/webproject/db/DBConnection.java
@@ -9,7 +9,7 @@ public class DBConnection {
     public static Connection getConnection(){
         Connection conn = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             conn = DriverManager.getConnection(url,username,password);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
